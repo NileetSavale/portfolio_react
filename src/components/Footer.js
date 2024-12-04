e272ace659912bbd9324301c2919613fce626bad
Nileet Savale <78837962+NileetSavale@users.noreply.github.com>
2024-12-03 22:38:21 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://linkedin.com/in/nileet"
+                href="https://www.linkedin.com/in/nileet/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
